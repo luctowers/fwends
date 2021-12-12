beb99da1bf6d1d69655c9f13e0f3dabf18b9e903
Lucas Towers <luctowers@gmail.com>
2021-12-11 22:02:46 -0800

    Format rust and add cargo watch

--- fwends-backend/src/routes/packs.rs
+++ fwends-backend/src/routes/packs.rs
@@ -1,32 +1,34 @@
-use serde::{Serialize};
-use warp::{Filter, filters::BoxedFilter, Reply};
+use serde::Serialize;
+use warp::{filters::BoxedFilter, Filter, Reply};
 
 #[derive(Serialize, Clone)]
 struct Pack {
     id: u64,
     name: String,
-    roleCount: u16,
-    stringCount: u16,
+    #[serde(rename = "roleCount")]
+    role_count: u16,
+    #[serde(rename = "stringCount")]
+    string_count: u16,
 }
 
 pub fn routes() -> BoxedFilter<(impl Reply,)> {
-  let packsData: Vec<Pack> = vec![
-      Pack {
-          id: 0,
-          name: String::from("Bar Pack One"),
-          roleCount: 3,
-          stringCount: 27
-      },
-      Pack {
-          id: 1,
-          name: String::from("Foo Pack Two"),
-          roleCount: 4,
-          stringCount: 32
-      },
-  ];
+    let pack_data: Vec<Pack> = vec![
+        Pack {
+            id: 0,
+            name: String::from("Bar Pack One"),
+            role_count: 3,
+            string_count: 27,
+        },
+        Pack {
+            id: 1,
+            name: String::from("Foo Pack Two"),
+            role_count: 4,
+            string_count: 32,
+        },
+    ];
 
-  warp::get()
-    .and(warp::path("packs"))
-    .map(move || warp::reply::json(&packsData))
-    .boxed()
+    warp::get()
+        .and(warp::path("packs"))
+        .map(move || warp::reply::json(&pack_data))
+        .boxed()
 }
