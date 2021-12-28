16d8f780ae36c5284e52b8b5d2b76e959f3e408d
Lucas Towers <luctowers@gmail.com>
2021-12-28 12:32:35 -0800

    Add underline to nav

--- fwends-frontend/src/components/Header.jsx
+++ fwends-frontend/src/components/Header.jsx
@@ -1,23 +1,41 @@
 export default App;
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuthStatus, authClear } from "../api/auth";
 import { LockIcon } from "@primer/octicons-react";
 
 function App() {
   const authenticated = useAuthStatus();
+  const location = useLocation();
+  const navItems = [
+    ["/", "FWENDS"],
+    ["/pieces", "PIECES"],
+    ["/packs", "PACKS"],
+  ];
   return (
-    <div className='p-6 flex items-center justify-center sm:justify-start space-x-4 sm:space-x-6 md:space-x-8'>
-      <Link className='font-bold' to='/'>
-        FWENDS
-      </Link>
-      <Link to='/pieces'>
-        PIECES
-      </Link>
-      <Link to='/packs'>
-        PACKS
-      </Link>
+    <div className='p-6 flex items-center justify-center sm:justify-start space-x-4 sm:space-x-6 md:space-x-8 underline-offset-1'>
+      {
+        navItems.map(([path,label]) =>
+          <Link
+            key={path} 
+            to={path}
+            className={
+              (
+                path == "/" ? "font-bold" : ""
+              ) +
+              (
+                (
+                  path == "/" ? 
+                    location.pathname == "/" :
+                    location.pathname.startsWith(path)
+                ) ? " underline" : ""
+              )
+            }>
+            {label}
+          </Link>
+        )
+      }
       {authenticated && 
         <button onClick={authClear}>
           <LockIcon />
