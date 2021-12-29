72aeb55190c2f520f99a637aad841e44e049ec51
Lucas Towers <luctowers@gmail.com>
2021-12-28 17:37:53 -0800

    Create a cool contenteditable input span

--- fwends-frontend/src/components/PackView.jsx
+++ fwends-frontend/src/components/PackView.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import SpanInput from "./SpanInput";
 
 export default function PackView() {
   const {packId} = useParams();
   const titleRef = useRef();
   useEffect(() => {
+    console.log(titleRef);
     if (packId == "new" && titleRef.current) {
       titleRef.current.focus();
     }
   }, [packId]);
   if (packId == "new") {
-    return <input ref={titleRef} placeholder='Pack Title' type='text' className='appearance-none focus:outline-none text-xl' />;
+    return (
+      <p className="text-xl tracking-wide">
+        <Link to='/packs' className="hover:underline decoration-1">packs</Link>
+        {" / "}
+        <SpanInput ref={titleRef} placeholder='Title' />
+      </p>
+    );
   }
 }
---
+++ fwends-frontend/src/components/SpanInput.jsx
@@ -0,0 +1,36 @@
+import React, { forwardRef, useEffect, useRef } from "react";
+
+export default forwardRef(function SpanInput(props, ref) {
+  const { className, contentEditable, onChange, onEnter, placeholder } = props;
+  const internalRef = useRef();
+  useEffect(() => {
+    ref.current = internalRef.current;
+  });
+  function handleChange() {
+    if (ref.current.innerText.trim() === "") {
+      ref.current.innerText = "";
+    }
+    // TODO: prevent extraneous onChange calls
+    if (onChange) {
+      onChange(ref.current.innerText);
+    }
+  }
+  function handleKeyDown(event) {
+    if(event.key === "Enter"){
+      event.preventDefault();
+      if (onEnter) {
+        onEnter();
+      }
+    }
+  }
+  return (
+    <span
+      ref={internalRef}
+      onInput={handleChange}
+      onBlur={handleChange}
+      onKeyDown={handleKeyDown}
+      className={"span-input " + (className || "")}
+      contentEditable={!(contentEditable === false)}
+      placeholder={placeholder} />
+  );
+});
