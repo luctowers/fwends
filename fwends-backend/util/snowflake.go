62c14ff07ca08d36dc72d1f1a28f369509ec2932
Lucas Towers <luctowers@gmail.com>
2021-12-30 16:19:00 -0800

    Add snowflake id implementation

---
+++ fwends-backend/util/snowflake.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"errors"
+	"sync/atomic"
+	"time"
+)
+
+// see https://en.wikipedia.org/wiki/Snowflake_ID
+
+// TODO: make these configurable
+const timestampBits = 41
+const machineBits = 10
+const sequenceBits = 12
+
+type SnowflakeGenerator struct {
+	sequence int64
+	base     int64
+}
+
+func NewSnowflakeGenerator(machine int64) (*SnowflakeGenerator, error) {
+	g := new(SnowflakeGenerator)
+	g.sequence = 0
+	if machine >= (1 << machineBits) {
+		return nil, errors.New("snowflake machine id out of range")
+	}
+	g.base = machine << sequenceBits
+	return g, nil
+}
+
+func (g *SnowflakeGenerator) GenID() int64 {
+	id := g.base
+	seq := atomic.AddInt64(&g.sequence, 1) - 1
+	id |= seq & ((1 << (sequenceBits + 1)) - 1)
+	id |= time.Now().UnixMilli() << sequenceBits << machineBits
+	return id
+}
