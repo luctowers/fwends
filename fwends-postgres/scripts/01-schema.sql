e1697fc79dbdc7136cb11589569f09b108e0b660
Lucas Towers <luctowers@gmail.com>
2022-01-07 01:36:15 -0800

    Update sql schema for pack images and sounds

--- fwends-postgres/scripts/01-schema.sql
+++ fwends-postgres/scripts/01-schema.sql
@@ -6,3 +6,21 @@ CREATE TABLE packs (
 	id BIGINT PRIMARY KEY,
 	title VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE packImages (
+	packId BIGINT,
+	roleId VARCHAR(63) NOT NULL,
+	stringId VARCHAR(63) NOT NULL,
+	extension VARCHAR(3) NOT NULL,
+	FOREIGN KEY (packId) REFERENCES packs(id),
+	PRIMARY KEY(packId, roleId, stringId)
+);
+
+CREATE TABLE packSounds (
+	packId BIGINT,
+	roleId VARCHAR(63) NOT NULL,
+	stringId VARCHAR(63) NOT NULL,
+	extension VARCHAR(3) NOT NULL,
+	FOREIGN KEY (packId) REFERENCES packs(id),
+	PRIMARY KEY(packId, roleId, stringId)
+);
