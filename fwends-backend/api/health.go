881cb4679f9447f7e3f3c5bf1b5d13a948de9583
Lucas Towers <luctowers@gmail.com>
2022-01-10 17:10:21 -0800

    Convert health api to use decorated handlers

--- fwends-backend/api/health.go
+++ fwends-backend/api/health.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"fwends-backend/util"
 	"net/http"
 	"sync"
 	"time"
@@ -26,71 +27,75 @@ func HealthCheck(db *sql.DB, rdb *redis.Client, s3c *s3.Client) httprouter.Handl
 	type responseBody struct {
 		Services serviceInfo `json:"services"`
 	}
-	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	return util.WrapDecoratedHandle(
+		func(w http.ResponseWriter, r *http.Request, _ httprouter.Params, logger *log.Entry) (int, error) {
 
-		// context that times out after 3 seconds, or when finish is called
-		ctx, finish := context.WithTimeout(context.Background(), time.Duration(3*time.Second))
-		defer finish()
-
-		// create wait group that will call finish when all services have responded
-		var wg sync.WaitGroup
-		wg.Add(3)
-		go func() {
+			// context that times out after 3 seconds, or when finish is called
+			ctx, finish := context.WithTimeout(context.Background(), time.Duration(3*time.Second))
 			defer finish()
-			wg.Wait()
-		}()
 
-		// ping postgres
-		cdb := make(chan bool)
-		go func() {
-			defer wg.Done()
-			err := db.PingContext(ctx)
-			if err != nil {
-				log.WithError(err).Error("Failed to ping postgres")
+			// create wait group that will call finish when all services have responded
+			var wg sync.WaitGroup
+			wg.Add(3)
+			go func() {
+				defer finish()
+				wg.Wait()
+			}()
+
+			// ping postgres
+			cdb := make(chan bool)
+			go func() {
+				defer wg.Done()
+				err := db.PingContext(ctx)
+				if err != nil {
+					logger.WithError(err).Error("Failed to ping postgres")
+				}
+				cdb <- err == nil
+			}()
+
+			// ping redis
+			crdb := make(chan bool)
+			go func() {
+				defer wg.Done()
+				cmd := rdb.Ping(ctx)
+				_, err := cmd.Result()
+				if err != nil {
+					logger.WithError(err).Error("Failed to ping redis")
+				}
+				crdb <- err == nil
+			}()
+
+			// check for bucket existance in s3
+			cs3c := make(chan bool)
+			go func() {
+				defer wg.Done()
+				_, err := s3c.ListBuckets(ctx, &s3.ListBucketsInput{})
+				if err != nil {
+					logger.WithError(err).Error("Failed to list s3 buckets")
+				}
+				cs3c <- err == nil
+			}()
+
+			var resbody responseBody
+		loop:
+			for {
+				select {
+				// status received from service
+				case resbody.Services.Postgres = <-cdb:
+				case resbody.Services.Redis = <-crdb:
+				case resbody.Services.S3 = <-cs3c:
+				// all service statuses rececived or timed out
+				case <-ctx.Done():
+					json.NewEncoder(w).Encode(resbody)
+					break loop
+				// request cancelled
+				case <-r.Context().Done():
+					break loop
+				}
 			}
-			cdb <- err == nil
-		}()
 
-		// ping redis
-		crdb := make(chan bool)
-		go func() {
-			defer wg.Done()
-			cmd := rdb.Ping(ctx)
-			_, err := cmd.Result()
-			if err != nil {
-				log.WithError(err).Error("Failed to ping redis")
-			}
-			crdb <- err == nil
-		}()
+			return http.StatusOK, nil
 
-		// check for bucket existance in s3
-		cs3c := make(chan bool)
-		go func() {
-			defer wg.Done()
-			_, err := s3c.ListBuckets(ctx, &s3.ListBucketsInput{})
-			if err != nil {
-				log.WithError(err).Error("Failed to list s3 buckets")
-			}
-			cs3c <- err == nil
-		}()
-
-		var resbody responseBody
-	loop:
-		for {
-			select {
-			// status received from service
-			case resbody.Services.Postgres = <-cdb:
-			case resbody.Services.Redis = <-crdb:
-			case resbody.Services.S3 = <-cs3c:
-			// all service statuses rececived or timed out
-			case <-ctx.Done():
-				json.NewEncoder(w).Encode(resbody)
-				break loop
-			// request cancelled
-			case <-r.Context().Done():
-				break loop
-			}
-		}
-
-	}
+		},
+	)
 }
