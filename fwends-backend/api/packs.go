8757cd0c2488c388ff9f1eeaf0db80434671d531
Lucas Towers <luctowers@gmail.com>
2022-01-21 17:29:58 -0800

    Update upload pack resource api to use new handler system

--- fwends-backend/api/packs.go
+++ fwends-backend/api/packs.go
@@ -1,13 +1,22 @@
 package api
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"fwends-backend/config"
 	"fwends-backend/handler"
 	"fwends-backend/util"
 	"net/http"
+	"regexp"
+	"strconv"
+
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/lib/pq"
+	"go.uber.org/zap"
 )
 
 /*
@@ -86,15 +95,15 @@ curl -X DELETE http://localhost:8080/api/packs/6882582496895041536/role/string
 //
 // Creates an new pack with a title and returns the id.
 func CreatePack(db *sql.DB, idgen *util.SnowflakeGenerator) handler.Handler {
-	return &CreatePackHandler{db, idgen}
+	return &createPackHandler{db, idgen}
 }
 
-type CreatePackHandler struct {
+type createPackHandler struct {
 	db    *sql.DB
 	idgen *util.SnowflakeGenerator
 }
 
-func (h *CreatePackHandler) Handle(i handler.Input) (int, error) {
+func (h *createPackHandler) Handle(i handler.Input) (int, error) {
 	// decode request body
 	decoder := json.NewDecoder(i.Request.Body)
 	var reqbody struct {
@@ -221,14 +230,14 @@ func (h *CreatePackHandler) Handle(i handler.Input) (int, error) {
 //
 // Updates a pack's title.
 func UpdatePack(db *sql.DB) handler.Handler {
-	return &UpdatePackHandler{db}
+	return &updatePackHandler{db}
 }
 
-type UpdatePackHandler struct {
+type updatePackHandler struct {
 	db *sql.DB
 }
 
-func (h *UpdatePackHandler) Handle(i handler.Input) (int, error) {
+func (h *updatePackHandler) Handle(i handler.Input) (int, error) {
 	packID := i.Params.ByName("pack_id")
 
 	// decode request body
@@ -263,183 +272,266 @@ func (h *UpdatePackHandler) Handle(i handler.Input) (int, error) {
 	return http.StatusOK, nil
 }
 
-// // PUT /api/packs/:pack_id/:role_id/:string_id
-// //
-// // Adds or replaces a image or audio pack resource.
-// func UploadPackResource(cfg *config.Config, logger *zap.Logger, db *sql.DB, s3c *s3.Client, snowflake *util.SnowflakeGenerator) httprouter.Handle {
-// 	return util.WrapDecoratedHandle(
-// 		cfg, logger,
-// 		func(w http.ResponseWriter, r *http.Request, ps httprouter.Params, logger *zap.Logger) (int, error) {
+// PUT /api/packs/:pack_id/:role_id/:string_id
+//
+// Adds or replaces a image or audio pack resource.
+func UploadPackResource(cfg *config.Config, db *sql.DB, s3c *s3.Client, idgen *util.SnowflakeGenerator) handler.Handler {
+	return &uploadPackResourceHandler{idgen, packResourceHandler{cfg, db, s3c}}
+}
 
-// 			// get path params
-// 			packID := ps.ByName("pack_id")
-// 			roleID := ps.ByName("role_id")
-// 			stringID := ps.ByName("string_id")
+type packResourceHandler struct {
+	cfg *config.Config
+	db  *sql.DB
+	s3c *s3.Client
+}
 
-// 			// validation
-// 			if !stringIDRegex.MatchString(roleID) {
-// 				return http.StatusBadRequest, fmt.Errorf("failed to validate role id: %v", roleID)
-// 			}
-// 			if !stringIDRegex.MatchString(stringID) {
-// 				return http.StatusBadRequest, fmt.Errorf("failed to validate string id: %v", stringID)
-// 			}
+type uploadPackResourceHandler struct {
+	idgen *util.SnowflakeGenerator
+	packResourceHandler
+}
 
-// 			// determine whether upload is audio or image
-// 			contentType := r.Header.Get("Content-Type")
-// 			resourceClass, err := deriveResourceClass(contentType)
-// 			if err != nil {
-// 				return http.StatusBadRequest, err
-// 			}
+func (h *uploadPackResourceHandler) Handle(i handler.Input) (int, error) {
+	packID := i.Params.ByName("pack_id")
+	roleID := i.Params.ByName("role_id")
+	stringID := i.Params.ByName("string_id")
 
-// 			// loop that will retry if transaction serialization anomaly occurs
-// 			resourceID := ""
-// 			transactionCommited := false
-// 			for {
+	// validation
+	if !packResourceIDRegex.MatchString(roleID) {
+		return http.StatusBadRequest, fmt.Errorf("failed to validate role id: %v", roleID)
+	}
+	if !packResourceIDRegex.MatchString(stringID) {
+		return http.StatusBadRequest, fmt.Errorf("failed to validate string id: %v", stringID)
+	}
 
-// 				// begin a new transcation
-// 				tx, err := db.BeginTx(r.Context(), &sql.TxOptions{
-// 					Isolation: sql.LevelSerializable,
-// 				})
-// 				if err != nil {
-// 					return http.StatusInternalServerError, err
-// 				}
-// 				defer tx.Rollback()
+	// determine whether upload is audio or image
+	contentType := i.Request.Header.Get("Content-Type")
+	resourceClass, err := derivePackResourceClass(contentType)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
 
-// 				// check whether pack exists and previous resource id exist
-// 				rows, err := db.QueryContext(r.Context(),
-// 					`
-// 					SELECT pack_resources.resource_id
-// 					FROM packs
-// 						LEFT OUTER JOIN pack_resources ON
-// 							pack_resources.pack_id = $1 AND
-// 							pack_resources.role_id = $2 AND
-// 							pack_resources.string_id = $3 AND
-// 							pack_resources.resource_class = $4
-// 					WHERE packs.pack_id = $1
-// 					`,
-// 					packID, roleID, stringID, resourceClass,
-// 				)
-// 				if err != nil {
-// 					return http.StatusInternalServerError, err
-// 				}
-// 				defer rows.Close()
-// 				var previousResourceID sql.NullInt64
-// 				if !rows.Next() {
-// 					// no row returned, the pack does not exist
-// 					return http.StatusNotFound, nil
-// 				} else {
-// 					rows.Scan(&previousResourceID)
-// 				}
-// 				rows.Close()
+	// check whether pack exists
+	packExists, err := h.packExists(i.Request.Context(), packID)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	if !packExists {
+		return http.StatusNotFound, nil
+	}
 
-// 				// if resource has not been uploaded yet
-// 				if resourceID == "" {
-// 					// defer prune the resource if tranacion does not complete
-// 					defer func() {
-// 						if !transactionCommited {
-// 							go func() {
-// 								err := pruneResource(context.Background(), logger, cfg, db, s3c, resourceID)
-// 								if err != nil {
-// 									logger.With(zap.Error(err)).Warn("failed to prune resource")
-// 								}
-// 							}()
-// 						}
-// 					}()
-// 					// insert row to mark possble existance of resource in s3
-// 					// note: this is intentionally not part of the transaction
-// 					resourceID = strconv.FormatInt(snowflake.GenID(), 10)
-// 					_, err = db.ExecContext(r.Context(),
-// 						"INSERT INTO resources (resource_id) VALUES ($1)",
-// 						resourceID,
-// 					)
-// 					if err != nil {
-// 						return http.StatusInternalServerError, err
-// 					}
-// 					// upload resource to s3
-// 					_, err = s3c.PutObject(r.Context(), &s3.PutObjectInput{
-// 						Bucket:        &cfg.S3.MediaBucket,
-// 						Key:           &resourceID,
-// 						Body:          r.Body,
-// 						ContentLength: r.ContentLength,
-// 						ContentType:   &contentType,
-// 					}, s3.WithAPIOptions(
-// 						v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware,
-// 					))
-// 					if err != nil {
-// 						return http.StatusInternalServerError, err
-// 					} else {
-// 						logger.With(zap.String("key", resourceID)).Info("uploaded resource to s3")
-// 					}
-// 				}
+	// defer prune the resource, in-case the transaction does not complete
+	resourceID := strconv.FormatInt(h.idgen.GenID(), 10)
+	transactionCommited := false
+	defer func() {
+		if !transactionCommited {
+			go func() {
+				err := h.pruneResource(context.Background(), resourceID)
+				if err != nil {
+					i.Logger.With(zap.Error(err)).Warn("failed to prune resource")
+				}
+			}()
+		}
+	}()
 
-// 				// determine whether to use and update or insert statement
-// 				var putResourceStatement string
-// 				if previousResourceID.Valid {
-// 					putResourceStatement = `
-// 						UPDATE pack_resources
-// 							SET resource_id = $5
-// 						WHERE
-// 							pack_id = $1 AND
-// 							role_id = $2 AND
-// 							string_id = $3 AND
-// 							resource_class = $4
-// 					`
-// 				} else {
-// 					putResourceStatement = `
-// 						INSERT INTO pack_resources
-// 							(pack_id, role_id, string_id, resource_class, resource_id)
-// 						VALUES
-// 							($1, $2, $3, $4, $5)
-// 					`
-// 				}
+	// upload the resource to s3
+	h.uploadResource(i.Request, resourceID)
 
-// 				// put pack_resource row
-// 				result, err := db.ExecContext(r.Context(),
-// 					putResourceStatement,
-// 					packID, roleID, stringID, resourceClass, resourceID,
-// 				)
-// 				if err != nil {
-// 					if isRetryableSerializationFailure(err) {
-// 						continue
-// 					}
-// 					return http.StatusInternalServerError, err
-// 				}
-// 				rowsAffected, _ := result.RowsAffected()
-// 				if rowsAffected != 1 {
-// 					return http.StatusInternalServerError, errors.New("inconsistent rows affected")
-// 				}
+	// loop that will retry if transaction serialization anomaly occurs
+	for !transactionCommited {
+		transactionCommited, err = h.updatePackResourceID(i.Request.Context(),
+			packID, roleID, stringID, resourceClass, resourceID,
+		)
+		if err != nil {
+			return http.StatusInternalServerError, err
+		}
+	}
 
-// 				// commit transaction
-// 				err = tx.Commit()
-// 				if err != nil {
-// 					if isRetryableSerializationFailure(err) {
-// 						continue
-// 					}
-// 					return http.StatusInternalServerError, err
-// 				}
-// 				transactionCommited = true
+	// repond with new resource id
+	i.Response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(i.Response).Encode(resourceID)
+	return http.StatusOK, nil
+}
 
-// 				// asynchronously attempt to prune old resource
-// 				if previousResourceID.Valid {
-// 					go func() {
-// 						id := strconv.FormatInt(previousResourceID.Int64, 10)
-// 						err := pruneResource(context.Background(), logger, cfg, db, s3c, id)
-// 						if err != nil {
-// 							logger.With(zap.Error(err)).Warn("failed to prune resource")
-// 						}
-// 					}()
-// 				}
+func (h *uploadPackResourceHandler) packExists(ctx context.Context, packID string) (bool, error) {
+	rows, err := h.db.QueryContext(ctx,
+		"SELECT 1 FROM packs WHERE pack_id = $1", packID,
+	)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	return rows.Next(), nil
+}
 
-// 				// repond with new resource id
-// 				w.Header().Set("Content-Type", "application/json")
-// 				json.NewEncoder(w).Encode(resourceID)
-// 				return http.StatusOK, nil
+func (h *uploadPackResourceHandler) uploadResource(r *http.Request, resourceID string) error {
+	// insert row to mark possble existance of resource in s3
+	_, err := h.db.ExecContext(r.Context(),
+		"INSERT INTO resources (resource_id) VALUES ($1)", resourceID,
+	)
+	if err != nil {
+		return err
+	}
 
-// 			}
+	// upload resource to s3
+	contentType := r.Header.Get("Content-Type")
+	_, err = h.s3c.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:        &h.cfg.S3.MediaBucket,
+		Key:           &resourceID,
+		Body:          r.Body,
+		ContentLength: r.ContentLength,
+		ContentType:   &contentType,
+	}, s3.WithAPIOptions(
+		v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware,
+	))
+	return err
+}
 
-// 		},
-// 	)
-// }
+func (h *uploadPackResourceHandler) updatePackResourceID(
+	ctx context.Context, packID string, roleID string, stringID string, resourceClass string, resourceID string,
+) (bool, error) {
+	// begin a new transcation
+	tx, err := h.db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+	})
+	if err != nil {
+		return false, err
+	}
+	defer tx.Rollback()
+
+	// check whether pack exists and previous resource id exist
+	rows, err := tx.QueryContext(ctx,
+		`
+		SELECT pack_resources.resource_id
+		FROM packs
+			LEFT OUTER JOIN pack_resources ON
+				pack_resources.pack_id = $1 AND
+				pack_resources.role_id = $2 AND
+				pack_resources.string_id = $3 AND
+				pack_resources.resource_class = $4
+		WHERE packs.pack_id = $1
+		`,
+		packID, roleID, stringID, resourceClass,
+	)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	var previousResourceID sql.NullInt64
+	if !rows.Next() {
+		// no row returned, the pack does not exist
+		// TODO: have a specialized
+		return false, errors.New("pack not found")
+	} else {
+		rows.Scan(&previousResourceID)
+	}
+	rows.Close()
+
+	// determine whether to use and update or insert statement
+	var putResourceStatement string
+	if previousResourceID.Valid {
+		putResourceStatement = `
+			UPDATE pack_resources
+				SET resource_id = $5
+			WHERE
+				pack_id = $1 AND
+				role_id = $2 AND
+				string_id = $3 AND
+				resource_class = $4
+		`
+	} else {
+		putResourceStatement = `
+			INSERT INTO pack_resources
+				(pack_id, role_id, string_id, resource_class, resource_id)
+			VALUES
+				($1, $2, $3, $4, $5)
+		`
+	}
+
+	// put pack_resource row
+	result, err := tx.ExecContext(ctx,
+		putResourceStatement,
+		packID, roleID, stringID, resourceClass, resourceID,
+	)
+	if err != nil {
+		if isRetryableSerializationFailure(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected != 1 {
+		return false, errors.New("inconsistent rows affected")
+	}
+
+	// commit transaction
+	err = tx.Commit()
+	if err != nil {
+		if isRetryableSerializationFailure(err) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	// asynchronously attempt to prune old resource
+	if previousResourceID.Valid {
+		go func() {
+			id := strconv.FormatInt(previousResourceID.Int64, 10)
+			h.pruneResource(context.Background(), id)
+		}()
+	}
+
+	return true, nil
+}
+
+func (h *packResourceHandler) pruneResource(ctx context.Context, id string) error {
+	// begin a new transcation
+	tx, err := h.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// atttempt delete row from resources tables
+	result, err := tx.ExecContext(ctx, "DELETE FROM resources WHERE resource_id = $1", id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// if a row was deleted, move it to pruned_resources and commit and start new transaction
+	if rowsAffected == 1 {
+		_, err := tx.ExecContext(ctx, "INSERT INTO pruned_resources (resource_id) VALUES ($1)", id)
+		if err != nil {
+			return err
+		}
+	}
+
+	// commit transaction
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	// delete from s3
+	_, err = h.s3c.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: &h.cfg.S3.MediaBucket,
+		Key:    &id,
+	})
+	if err != nil {
+		return err
+	}
+
+	// attempt delete row from pruned_resources tables
+	_, err = h.db.ExecContext(ctx, "DELETE FROM pruned_resources WHERE resource_id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // // DELETE /api/packs/:pack_id
 // //
@@ -485,89 +577,37 @@ func (h *UpdatePackHandler) Handle(i handler.Input) (int, error) {
 
 // // HELPERS
 
-// var stringIDRegex = regexp.MustCompile(`^[a-z0-9_]{1,63}$`)
+var packResourceIDRegex = regexp.MustCompile(`^[a-z0-9_]{1,63}$`)
 
-// func isRetryableSerializationFailure(err error) bool {
-// 	if pqErr, ok := err.(*pq.Error); ok {
-// 		return pqErr.Code.Name() == "serialization_failure"
-// 	}
-// 	return false
-// }
+func isRetryableSerializationFailure(err error) bool {
+	if pqErr, ok := err.(*pq.Error); ok {
+		return pqErr.Code.Name() == "serialization_failure"
+	}
+	return false
+}
 
-// func deriveResourceClass(contentType string) (string, error) {
-// 	switch contentType {
-// 	// image content types
-// 	case "image/webp":
-// 		fallthrough
-// 	case "image/jpeg":
-// 		fallthrough
-// 	case "image/png":
-// 		fallthrough
-// 	case "image/svg+xml":
-// 		return "image", nil
-// 	// audio content types
-// 	case "audio/aac":
-// 		fallthrough
-// 	case "audio/mpeg":
-// 		fallthrough
-// 	case "audio/wav":
-// 		fallthrough
-// 	case "audio/flac":
-// 		return "audio", nil
-// 	// unknown content types
-// 	default:
-// 		return "", errors.New("unsupported pack resource content type")
-// 	}
-// }
-
-// func pruneResource(ctx context.Context, logger *zap.Logger, cfg *config.Config, db *sql.DB, s3c *s3.Client, id string) error {
-// 	// begin a new transcation
-// 	tx, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer tx.Rollback()
-
-// 	// atttempt delete row from resources tables
-// 	result, err := db.ExecContext(ctx, "DELETE FROM resources WHERE resource_id = $1", id)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	rowsAffected, err := result.RowsAffected()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	// if a row was deleted, move it to pruned_resources and commit and start new transaction
-// 	if rowsAffected == 1 {
-// 		_, err := db.ExecContext(ctx, "INSERT INTO pruned_resources (resource_id) VALUES ($1)", id)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-
-// 	// commit transaction
-// 	err = tx.Commit()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// attempt delete row from pruned_resources tables
-// 	_, err = db.ExecContext(ctx, "DELETE FROM pruned_resources WHERE resource_id = $1", id)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// delete from s3
-// 	_, err = s3c.DeleteObject(ctx, &s3.DeleteObjectInput{
-// 		Bucket: &cfg.S3.MediaBucket,
-// 		Key:    &id,
-// 	})
-// 	if err != nil {
-// 		return err
-// 	} else {
-// 		logger.With(zap.String("key", key)).Info("deleted resource from s3")
-// 	}
-
-// 	return nil
-
-// }
+func derivePackResourceClass(contentType string) (string, error) {
+	switch contentType {
+	// image content types
+	case "image/webp":
+		fallthrough
+	case "image/jpeg":
+		fallthrough
+	case "image/png":
+		fallthrough
+	case "image/svg+xml":
+		return "image", nil
+	// audio content types
+	case "audio/aac":
+		fallthrough
+	case "audio/mpeg":
+		fallthrough
+	case "audio/wav":
+		fallthrough
+	case "audio/flac":
+		return "audio", nil
+	// unknown content types
+	default:
+		return "", errors.New("unsupported pack resource content type")
+	}
+}
