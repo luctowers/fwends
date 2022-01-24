497d0d426b6222c4c5696021dbf335b119fbd7f3
Lucas Towers <luctowers@gmail.com>
2022-01-23 18:31:11 -0800

    Update tests for new pack api

--- fwends-test/test_packs.py
+++ fwends-test/test_packs.py
@@ -1,269 +1,204 @@
 import hashlib
 import requests
 
+def test_pack_no_resources(backend):
+	pack_id = create_test_pack(backend, "Test Pack Update")
+	verify_pack_title(backend, pack_id, "Test Pack Update")
 
-def test_pack_crud(backend):
-	"""Test the POST, GET, PUT and DELETE methods of packs api."""
-
-	# create pack
-	pack = {"title":"Test Title!"}
-	response = requests.post(
-		backend + "/packs/",
-		json=pack
-	)
-	assert response.status_code == 200
-	response_data = response.json()
-	assert "id" in response_data
-	pack_id = response_data["id"]
-	assert isinstance(pack_id, str)
-	assert int(pack_id) >= 0
-
-	# get pack
-	response = requests.get(backend+"/packs/"+pack_id)
-	assert response.status_code == 200
-	response_data = response.json()
-	assert "title" in response_data
-	assert response_data["title"] == pack["title"]
-
-	# verify with list pack api
-	response = requests.get(backend+"/packs/")
-	assert response.status_code == 200
-	response_element = next((x for x in response.json() if x['id']==pack_id),None)
-	assert response_element == {
-		"id": pack_id,
-		"title": pack["title"],
-		"roleCount": 0,
-		"stringCount": 0
-	}
-
-	# update pack
-	updated_pack = {"title":"Updated Title!!"}
+	updated_pack = {"title":"New Updated Title!"}
 	response = requests.put(backend+"/packs/"+pack_id, json=updated_pack)
 	assert response.status_code == 200
+	verify_pack_title(backend, pack_id, "New Updated Title!")
 
-	# get pack again
-	response = requests.get(backend+"/packs/"+pack_id)
-	assert response.status_code == 200
-	response_data = response.json()
-	assert "title" in response_data
-	assert response_data["title"] == updated_pack["title"]
-
-	# verify with list pack api
-	response = requests.get(backend+"/packs/")
-	assert response.status_code == 200
-	response_element = next((x for x in response.json() if x['id']==pack_id),None)
-	assert response_element == {
-		"id": pack_id,
-		"title": updated_pack["title"],
-		"roleCount": 0,
-		"stringCount": 0
-	}
-
-	# delete pack
 	response = requests.delete(backend+"/packs/"+pack_id)
 	assert response.status_code == 200
 
-	# get pack not found
 	response = requests.get(backend+"/packs/"+pack_id)
 	assert response.status_code == 404
 
+def test_pack_hash(backend, media):
+	pack_id = create_test_pack(backend, "Test Pack Hash")
+	expected_hash = hashlib.sha256(b'').hexdigest()
+	verify_pack_hash(backend, pack_id, expected_hash)
 
-def test_pack_resource_crud(backend, media):
+	populate_test_pack_resources(backend, media, pack_id)
+	expected_hash = hashlib.sha256(
+		'\x00bird\x01duck\x01eagle\x01robin\x00mammal\x01cat\x01dog\x01tiger'.encode('utf-8')
+	).hexdigest()
+	verify_pack_hash(backend, pack_id, expected_hash)
 
-	# create pack
-	pack = {"title":"Test Title!"}
+	response = requests.delete(backend+"/packs/"+pack_id+"/bird/robin")
+	assert response.status_code == 200
+	expected_hash = hashlib.sha256(
+		'\x00bird\x01duck\x01eagle\x00mammal\x01cat\x01dog\x01tiger'.encode('utf-8')
+	).hexdigest()
+	verify_pack_hash(backend, pack_id, expected_hash)
+
+	response = requests.delete(backend+"/packs/"+pack_id+"/mammal")
+	assert response.status_code == 200
+	expected_hash = hashlib.sha256(
+		'\x00bird\x01duck\x01eagle'.encode('utf-8')
+	).hexdigest()
+	verify_pack_hash(backend, pack_id, expected_hash)
+
+	response = requests.delete(backend+"/packs/"+pack_id+"/bird")
+	assert response.status_code == 200
+	expected_hash = hashlib.sha256(b'').hexdigest()
+	verify_pack_hash(backend, pack_id, expected_hash)
+
+	populate_test_pack_resources(backend, media, pack_id)
+	expected_hash = hashlib.sha256(
+		'\x00bird\x01duck\x01eagle\x01robin\x00mammal\x01cat\x01dog\x01tiger'.encode('utf-8')
+	).hexdigest()
+	verify_pack_hash(backend, pack_id, expected_hash)
+
+	response = requests.delete(backend+"/packs/"+pack_id)
+	assert response.status_code == 200
+
+
+def test_pack_role_and_string_counts(backend, media):
+	pack_id = create_test_pack(backend, "Test Pack Counts")
+	verify_pack_counts(backend, pack_id, role_count=0, string_count=0)
+
+	populate_test_pack_resources(backend, media, pack_id)
+	verify_pack_counts(backend, pack_id, role_count=2, string_count=6)
+
+	response = requests.delete(backend+"/packs/"+pack_id+"/mammal/cat")
+	assert response.status_code == 200
+	verify_pack_counts(backend, pack_id, role_count=2, string_count=5)
+
+	response = requests.delete(backend+"/packs/"+pack_id+"/mammal/dog")
+	assert response.status_code == 200
+	verify_pack_counts(backend, pack_id, role_count=2, string_count=4)
+
+	# double delete shouldn't change anything
+	response = requests.delete(backend+"/packs/"+pack_id+"/mammal/dog")
+	assert response.status_code == 200
+	verify_pack_counts(backend, pack_id, role_count=2, string_count=4)
+
+	# mammal role is implicitly removed because all of its string were deleted
+	response = requests.delete(backend+"/packs/"+pack_id+"/mammal/tiger")
+	assert response.status_code == 200
+	verify_pack_counts(backend, pack_id, role_count=1, string_count=3)
+
+	response = requests.delete(backend+"/packs/"+pack_id+"/bird")
+	assert response.status_code == 200
+	verify_pack_counts(backend, pack_id, role_count=0, string_count=0)
+
+	response = requests.delete(backend+"/packs/"+pack_id)
+	assert response.status_code == 200
+
+# HELPERS
+
+def create_test_pack(backend, title):
+	pack = {"title":title}
 	response = requests.post(backend + "/packs/", json=pack)
 	assert response.status_code == 200
 	response_data = response.json()
+	pack_hash = response_data["hash"]
+	# should sha256 of an empty input
+	assert pack_hash == hashlib.sha256(b'').hexdigest()
 	pack_id = response_data["id"]
-	pack_api = backend+"/packs/"+pack_id
-	pack_media = media+"/packs/"+pack_id
+	assert isinstance(pack_id, str)
+	return pack_id
 
+def populate_test_pack_resources(backend, media, pack_id):
+	pack_api = backend + "/packs/" + pack_id
 	# upload resources
-	with open("./resources/bird-duck.jpg", "rb") as file:
-		upload_resource(pack_api+"/bird/duck", file, "image/jpeg")
 	with open("./resources/bird-duck.aac", "rb") as file:
-		upload_resource(pack_api+"/bird/duck", file, "audio/aac")
+		duck_audio_id = upload_resource(pack_api+"/bird/duck", file, "audio/aac")
 	with open("./resources/bird-eagle.png", "rb") as file:
-		upload_resource(pack_api+"/bird/eagle", file, "image/png")
+		eagle_image_id = upload_resource(pack_api+"/bird/eagle", file, "image/png")
 	with open("./resources/bird-eagle.mp3", "rb") as file:
-		upload_resource(pack_api+"/bird/eagle", file, "audio/mpeg")
-	# omit robin image
-	# with open("./resources/bird-robin.jpg", "rb") as file:
-	# 	upload_resource(pack_api+"/bird/robin", file, "image/jpeg")
-	with open("./resources/bird-robin.mp3", "rb") as file:
-		upload_resource(pack_api+"/bird/robin", file, "audio/mpeg")
+		eagle_audio_id = upload_resource(pack_api+"/bird/eagle", file, "audio/mpeg")
+	with open("./resources/bird-robin.jpg", "rb") as file:
+		robin_image_id = upload_resource(pack_api+"/bird/robin", file, "image/jpeg")
 	with open("./resources/mammal-cat.jpg", "rb") as file:
-		upload_resource(pack_api+"/mammal/cat", file, "image/jpeg")
+		cat_image_id = upload_resource(pack_api+"/mammal/cat", file, "image/jpeg")
 	with open("./resources/mammal-cat.flac", "rb") as file:
-		upload_resource(pack_api+"/mammal/cat", file, "audio/flac")
+		cat_audio_id = upload_resource(pack_api+"/mammal/cat", file, "audio/flac")
 	with open("./resources/mammal-dog.webp", "rb") as file:
-		upload_resource(pack_api+"/mammal/dog", file, "image/webp")
-	with open("./resources/mammal-dog.m4a", "rb") as file:
-		upload_resource(pack_api+"/mammal/dog", file, "audio/aac")
+		dog_image_id = upload_resource(pack_api+"/mammal/dog", file, "image/webp")
 	with open("./resources/mammal-tiger.svg", "rb") as file:
-		upload_resource(pack_api+"/mammal/tiger", file, "image/svg+xml")
-	# omit tiger sound
-	# with open("./resources/mammal-tiger.wav", "rb") as file:
-	# 	upload_resource(pack_api+"/mammal/tiger", file, "audio/wav")
-
-	# verify resources have been uploaded
-	with open("./resources/bird-duck.jpg", "rb") as file:
-		verify_resource(pack_media+"/bird/duck/image", file, "image/jpeg")
+		tiger_image_id = upload_resource(pack_api+"/mammal/tiger", file, "image/svg+xml")
+	with open("./resources/mammal-tiger.wav", "rb") as file:
+		tiger_audio_id = upload_resource(pack_api+"/mammal/tiger", file, "audio/wav")
+	# verify resource integrity
 	with open("./resources/bird-duck.aac", "rb") as file:
-		verify_resource(pack_media+"/bird/duck/audio", file, "audio/aac")
+		verify_resource(media+"/"+duck_audio_id, file, "audio/aac")
 	with open("./resources/bird-eagle.png", "rb") as file:
-		verify_resource(pack_media+"/bird/eagle/image", file, "image/png")
+		verify_resource(media+"/"+eagle_image_id, file, "image/png")
 	with open("./resources/bird-eagle.mp3", "rb") as file:
-		verify_resource(pack_media+"/bird/eagle/audio", file, "audio/mpeg")
-	# omit robin image
-	# with open("./resources/bird-robin.jpg", "rb") as file:
-	# 	verify_resource(pack_media+"/bird/robin/image", file, "image/jpeg")
-	with open("./resources/bird-robin.mp3", "rb") as file:
-		verify_resource(pack_media+"/bird/robin/audio", file, "audio/mpeg")
+		verify_resource(media+"/"+eagle_audio_id, file, "audio/mpeg")
+	with open("./resources/bird-robin.jpg", "rb") as file:
+		verify_resource(media+"/"+robin_image_id, file, "image/jpeg")
 	with open("./resources/mammal-cat.jpg", "rb") as file:
-		verify_resource(pack_media+"/mammal/cat/image", file, "image/jpeg")
+		verify_resource(media+"/"+cat_image_id, file, "image/jpeg")
 	with open("./resources/mammal-cat.flac", "rb") as file:
-		verify_resource(pack_media+"/mammal/cat/audio", file, "audio/flac")
+		verify_resource(media+"/"+cat_audio_id, file, "audio/flac")
 	with open("./resources/mammal-dog.webp", "rb") as file:
-		verify_resource(pack_media+"/mammal/dog/image", file, "image/webp")
-	with open("./resources/mammal-dog.m4a", "rb") as file:
-		verify_resource(pack_media+"/mammal/dog/audio", file, "audio/aac")
+		verify_resource(media+"/"+dog_image_id, file, "image/webp")
 	with open("./resources/mammal-tiger.svg", "rb") as file:
-		verify_resource(pack_media+"/mammal/tiger/image", file, "image/svg+xml")
-	# omit tiger sound
-	# with open("./resources/mammal-tiger.wav", "rb") as file:
-	# 	verify_resource(pack_media+"/mammal/tiger/audio", file, "audio/wav")
-
-	# verify with get pack api
-	response = requests.get(pack_api)
+		verify_resource(media+"/"+tiger_image_id, file, "image/svg+xml")
+	with open("./resources/mammal-tiger.wav", "rb") as file:
+		verify_resource(media+"/"+tiger_audio_id, file, "audio/wav")
+	# verify resource ids in get pack api
+	response = requests.get(backend+"/packs/"+pack_id)
 	assert response.status_code == 200
-	assert response.json()["resources"] == {
-		"bird": {
-			"duck": {"image": True, "audio": True},
-			"eagle": {"image": True, "audio": True},
-			"robin": {"image": False, "audio": True},
+	assert response.json()['roles'] == [
+		{
+			"id":"bird",
+			"strings":[
+				{"id":"duck","audio":duck_audio_id},
+				{"id":"eagle","audio":eagle_audio_id,"image":eagle_image_id},
+				{"id":"robin","image":robin_image_id}
+			]
 		},
-		"mammal": {
-			"cat": {"image": True, "audio": True},
-			"dog": {"image": True, "audio": True},
-			"tiger": {"image": True, "audio": False}
+		{
+			"id":"mammal",
+			"strings":[
+				{"id":"cat","audio":cat_audio_id,"image":cat_image_id},
+				{"id":"dog","image":dog_image_id},
+				{"id":"tiger","audio":tiger_audio_id,"image":tiger_image_id}
+			]
 		}
-	}
+	]
 
-	# verify with list pack api
+def verify_pack_hash(backend, pack_id, expected_hash):
+	response = requests.get(backend+"/packs/"+pack_id)
+	assert response.status_code == 200
+	pack_summary = response.json()
+	assert pack_summary['hash'] == expected_hash
 	response = requests.get(backend+"/packs/")
 	assert response.status_code == 200
-	response_element = next((x for x in response.json() if x['id']==pack_id),None)
-	assert response_element == {
-		"id": pack_id,
-		"title": pack["title"],
-		"roleCount": 2, # mammal and bird
-		"stringCount": 4 # tiger and robin, not counted as resources were omitted
-	}
+	pack_list = response.json()
+	assert isinstance(pack_list, list)
+	filtered_data = list(filter(lambda p: p['id'] == pack_id, pack_list))
+	assert len(filtered_data) == 1
+	assert filtered_data[0]['hash'] == expected_hash
 
-	# delete cat string
-	response = requests.delete(pack_api+"/mammal/cat")
-	assert response.status_code == 200
-	# make sure tiger resources have been deleted
-	verify_resource_deleted(pack_media+"/mammal/cat/image")
-	verify_resource_deleted(pack_media+"/mammal/cat/audio")
-
-	# verify with get pack api
-	response = requests.get(pack_api)
-	assert response.status_code == 200
-	assert response.json()["resources"] == {
-		"bird": {
-			"duck": {"image": True, "audio": True},
-			"eagle": {"image": True, "audio": True},
-			"robin": {"image": False, "audio": True},
-		},
-		"mammal": {
-			# cat deleted
-			"dog": {"image": True, "audio": True},
-			"tiger": {"image": True, "audio": False}
-		}
-	}
-
-	# verify with list pack api
+def verify_pack_counts(backend, pack_id, role_count, string_count):
 	response = requests.get(backend+"/packs/")
 	assert response.status_code == 200
-	response_element = next((x for x in response.json() if x['id']==pack_id),None)
-	assert response_element == {
-		"id": pack_id,
-		"title": pack["title"],
-		"roleCount": 2, # mammal and bird
-		"stringCount": 3 # cat now deleted
-	}
+	pack_list = response.json()
+	assert isinstance(pack_list, list)
+	filtered_data = list(filter(lambda p: p['id'] == pack_id, pack_list))
+	assert len(filtered_data) == 1
+	assert filtered_data[0]['roleCount'] == role_count
+	assert filtered_data[0]['stringCount'] == string_count
 
-	# delete bird role
-	response = requests.delete(pack_api+"/bird")
+def verify_pack_title(backend, pack_id, title):
+	response = requests.get(backend+"/packs/"+pack_id)
 	assert response.status_code == 200
-	# make sure bird resources have been deleted
-	verify_resource_deleted(pack_media+"/bird/duck/image")
-	verify_resource_deleted(pack_media+"/bird/duck/audio")
-	verify_resource_deleted(pack_media+"/bird/eagle/image")
-	verify_resource_deleted(pack_media+"/bird/eagle/audio")
-	# omit robin image
-	# verify_resource_deleted(pack_media+"/bird/robin/image")
-	verify_resource_deleted(pack_media+"/bird/robin/audio")
-
-	# verify others mammals are still alive
-	with open("./resources/mammal-tiger.svg", "rb") as file:
-		verify_resource(pack_media+"/mammal/tiger/image", file, "image/svg+xml")
-	# omit tiger sound
-	# with open("./resources/mammal-tiger.wav", "rb") as file:
-	# 	verify_resource(pack_media+"/mammal/tiger/audio", file, "audio/wav")
-	with open("./resources/mammal-dog.webp", "rb") as file:
-		verify_resource(pack_media+"/mammal/dog/image", file, "image/webp")
-	with open("./resources/mammal-dog.m4a", "rb") as file:
-		verify_resource(pack_media+"/mammal/dog/audio", file, "audio/aac")
-
-	# replace dog with eagle to test updating resource
-	with open("./resources/bird-eagle.png", "rb") as file:
-		upload_resource(pack_api+"/mammal/dog", file, "image/png")
-	with open("./resources/bird-eagle.mp3", "rb") as file:
-		upload_resource(pack_api+"/mammal/dog", file, "audio/mpeg")
-	# verify resource has been updated
-	with open("./resources/bird-eagle.png", "rb") as file:
-		verify_resource(pack_media+"/mammal/dog/image", file, "image/png")
-	with open("./resources/bird-eagle.mp3", "rb") as file:
-		verify_resource(pack_media+"/mammal/dog/audio", file, "audio/mpeg")
-
-	# verify with get pack api
-	response = requests.get(pack_api)
-	assert response.status_code == 200
-	assert response.json()["resources"] == {
-		# birds deleted
-		"mammal": {
-			"dog": {"image": True, "audio": True},
-			"tiger": {"image": True, "audio": False}
-		}
-	}
-
-	# verify with list pack api
+	pack_summary = response.json()
+	assert pack_summary['title'] == title
 	response = requests.get(backend+"/packs/")
 	assert response.status_code == 200
-	response_element = next((x for x in response.json() if x['id']==pack_id),None)
-	assert response_element == {
-		"id": pack_id,
-		"title": pack["title"],
-		"roleCount": 1, # only mammal is left
-		"stringCount": 1 # only dog is left
-	}
-
-	# delete pack
-	response = requests.delete(pack_api)
-	assert response.status_code == 200
-	# make sure remaining pack resources have been deleted
-	verify_resource_deleted(pack_media+"/mammal/dog/image")
-	verify_resource_deleted(pack_media+"/mammal/dog/audio")
-	verify_resource_deleted(pack_media+"/mammal/tiger/image")
-	# omit tiger sound
-	# verify_resource_deleted(pack_media+"/mammal/tiger/audio")
-
-
-# HELPERS
+	pack_list = response.json()
+	assert isinstance(pack_list, list)
+	filtered_data = list(filter(lambda p: p['id'] == pack_id, pack_list))
+	assert len(filtered_data) == 1
+	assert filtered_data[0]['title'] == title
 
 def hash_file(file):
 	sha256 = hashlib.sha256()
@@ -284,6 +219,9 @@ def upload_resource(url, file, content_type):
 	file.seek(0)
 	response = requests.put(url, headers={"Content-Type":content_type}, data=file)
 	assert response.status_code == 200
+	resource_id = response.json()
+	assert isinstance(resource_id, str)
+	return resource_id
 
 def verify_resource(url, file, content_type):
 	file.seek(0)
